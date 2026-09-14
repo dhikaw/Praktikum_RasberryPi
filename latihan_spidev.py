d6d1d97b2df39dbd807cbe4e25c11dd2ca75e773
Dhika Wahyu Arfian Putra <dhikawahyuarfianputra@gmail.com>
2026-09-14 10:39:50 +0700

    Tulis pesan perubahan di sini

--- latihan_spidev.py
+++ latihan_spidev.py
@@ -1,11 +1,13 @@
 import spidev
 import time
+
 spi = spidev.SpiDev()
 spi.open(0, 1)
+
 try:
-while True:
-resp = spi.xfer2([0xAA])
-time.sleep(1)
-print("spi out: " + str(resp))
+    while True:
+        resp = spi.xfer2([0xAA])
+        time.sleep(1)
+        print("spi out: " + str(resp))
 except KeyboardInterrupt:
-spi.close()
+    spi.close()
